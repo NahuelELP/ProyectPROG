465aba92c64eaa016bd897437e71943668d3dd69
nahuel <lassosnahuel90@gmail.com>
2025-10-14 22:19:37 +0100

    CondicionalesUwu

---
+++ IF,else/IF,else/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame tu tiket, tenes?");
+            string Tiket = (Console.ReadLine());
+            Console.WriteLine("Y cuantos años tenes?");
+            int Edad = int.Parse(Console.ReadLine());
+
+            if (Tiket == "si" && Edad >= 15) //Ambas se tienen que cumplir con -----> || con esto solo se tiene que cumplir uno y ya entra al if
+            {
+                Console.WriteLine("Ok pasa");
+            }
+            else
+                {
+                Console.WriteLine("No podes pasar");
+                }
+        }
+    }
+}
---
+++ IF,else/IF,else/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("IF,else")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IF,else")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("4881493c-a37d-42be-9a8c-8875362d3867")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
