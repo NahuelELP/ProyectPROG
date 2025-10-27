4b7635a0aa2ce13647293de26b07762c6ef4e769
nahuel <lassosnahuel90@gmail.com>
2025-10-27 22:01:21 +0000

    NocheDEL27

---
+++ POO-ModificadoresAcceso/ModificadoresAcceso2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POO_ModificadoresAcceso;
+
+namespace ModificadoresAcceso2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario marto = new Usuario();
+            marto.nombre = "marto";
+            marto.IniciarSesion();
+        }
+    }
+}
---
+++ POO-ModificadoresAcceso/POO-ModificadoresAcceso/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ModificadoresAcceso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario nahuel = new Usuario();
+            nahuel.nombre = "nahuel";
+            nahuel.IniciarSesion();
+
+        }
+    }
+    public class Usuario
+    {
+        public string nombre;
+        public string email;
+        public string contraseña;
+
+        public void IniciarSesion()
+        {
+            Console.WriteLine("El usuario {0} acaba de iniciar sesion", nombre);
+        }
+        public void CerrarSesion()
+        {
+            Console.WriteLine("El usuario {0} acaba de iniciar sesion", nombre);
+        }
+        public void CambiarContraseña()
+        {
+            Console.WriteLine("El usuario {0} acaba de cambiar su contraseña", nombre);
+        }
+    }
+}
